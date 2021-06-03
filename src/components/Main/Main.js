cc591b05e168b1bf5e2e8a84673a5fd89c1b9c7d
Stas Smirnov <smistav@ya.ru>
2021-06-03 17:03:37 +0300

    add block Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Promo from "./Promo/Promo";
-function Main(){
-  return(
-<main className="main">
-    <Promo/>
-</main>
+function Main() {
+  return (
+    <main className="main">
+      <Promo />
+
+    </main>
   )
 }
 export default Main
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -2,31 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../logo.svg";
 import promo_img from "../../../images/ball_web.svg"
-function Promo(){
-  return(
+function Promo() {
+  return (
     <section className="promo">
-      <div className="promo__header">
-        <Link  to="/" className="promo__logo">
-          <img src={logo} alt="Логотип" className="logo"/>
-        </Link>
-        <div className="promo__auth">
-          <Link to="signup" className="promo__signup">
-            Регистрация
+      <div className="promo__container">
+        <div className="promo__header">
+          <Link to="/">
+            <img src={logo} alt="Логотип" className="logo" />
           </Link>
-          <Link to="signin" className="promo__signin">
-            Войти
+          <div className="promo__auth">
+            <Link to="signup" className="promo__signup">
+              Регистрация
           </Link>
+            <Link to="signin" className="promo__signin">
+              Войти
+          </Link>
+          </div>
         </div>
-      </div>
-      <div className="promo__main">
-        <div className="promo__heading">
-          <h1>Учебный проект студента факультета Веб-разработки.</h1>
-          <p>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        <div className="promo__main">
+          <div>
+            <h1 className="promo__heading">Учебный проект студента факультета Веб-разработки.</h1>
+            <p className="promo__paragraph">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+            <button className="promo__button">Узнать больше</button>
+          </div>
+          <img src={promo_img} alt="Картинка WEB земной шар" className="promo__img" />
         </div>
-        <img src={promo_img} alt="Картинка WEB земной шар" className="promo_img"/>
-      </div>
-      <div className="promo__footer">
-
+        <div className="promo__footer">
+        </div>
       </div>
     </section>
   )
