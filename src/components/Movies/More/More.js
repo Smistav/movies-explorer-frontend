a03a338e1f6ed72aa978d62b91d34c0b94ecae58
Stas Smirnov <smistav@ya.ru>
2021-06-08 18:17:06 +0300

    add More to Movies

---
+++ src/components/Movies/More/More.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function More() {
+  return (
+    <div className="more">
+      <button className="more__button">Еще</button>
+    </div>
+  )
+}
+export default More
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,13 +2,15 @@ import React from "react";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 import SearchForm from "./SearchForm/SearchForm";
 import Preloader from '../../components/Movies/Preloader/Preloader';
+import More from "./More/More";
 function Movies() {
   return (
     <section className="main movies">
       <div className="main__container">
         <SearchForm />
         <MoviesCardList />
-        <Preloader />
+        {/* <Preloader /> */}
+        <More />
       </div>
     </section>
   )
