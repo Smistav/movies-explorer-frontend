c36155206aeee84566cec69d641b7d7f80742586
Stas Smirnov <smistav@ya.ru>
2021-06-08 18:10:34 +0300

    add Preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 import SearchForm from "./SearchForm/SearchForm";
+import Preloader from '../../components/Movies/Preloader/Preloader';
 function Movies() {
   return (
     <section className="main movies">
       <div className="main__container">
         <SearchForm />
         <MoviesCardList />
+        <Preloader />
       </div>
     </section>
   )
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import MoviesCard from '../MoviesCard/MoviesCard';
-import Preloader from '../Preloader/Preloader';
 import cards from '../../../utils/cards';
 function MoviesCardList() {
   return (
     <section className="movie-card-list">
-      <div className="movie-card-list__container">
-        {cards.map((item) => (
-          <MoviesCard key={item._id} card={item} />
-        ))}
-      </div>
-      <Preloader />
+      {cards.map((item) => (
+        <MoviesCard key={item._id} card={item} />
+      ))}
     </section>
   )
 }
