fd57d7a1a75744fc4da1fecf5d708833fb38173b
Stas Smirnov <smistav@ya.ru>
2021-06-08 09:49:31 +0300

    add to Navigation icon

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from "../../logo.svg";
+import account_icon from "../../images/account-icon.svg";
 
 function Navigation() {
   return (
@@ -16,8 +17,9 @@ function Navigation() {
           Сохраненные фильмы
         </NavLink>
       </div>
-      <NavLink to="/profile" activeClassName="menu__link_active" className="menu__link">
+      <NavLink to="/profile" activeClassName="menu__link_active" className="menu__link menu__link_image">
         Аккаунт
+        <img src={account_icon} alt="иконка" className="menu__account-icon" />
       </NavLink>
     </nav>
   )
