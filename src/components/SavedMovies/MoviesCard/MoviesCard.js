17fb912ac1f5ef9baab401ad37bbd3c2ec9c892a
Stas Smirnov <smistav@ya.ru>
2021-06-09 11:48:02 +0300

    add to MoviesCard remove_icon

--- src/components/SavedMovies/MoviesCard/MoviesCard.js
+++ src/components/SavedMovies/MoviesCard/MoviesCard.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import '../MoviesCard/MoviesCard.css';
 
+function handleRemove(evt) {
+  evt.target.closest('figure')
+    .querySelector('.movies-card__remove')
+    .classList.add('movies-card__remove_active');
+}
+function handleRemoveOut(evt) {
+  evt.target.closest('figure')
+    .querySelector('.movies-card__remove')
+    .classList.remove('movies-card__remove_active');
+}
+
 function MoviesCard({ card }) {
   const time = ~~(card.time / 60) + 'ч ' + (card.time % 60) + 'м';
   return (
-    <figure className="movies-card">
+    <figure onMouseOver={handleRemove} onMouseOut={handleRemoveOut} className="movies-card">
       <img src={card.link} alt={card.name} className="movies-card__img" />
+      <span className="movies-card__mask"><i className="img__icon"></i></span>
       <figcaption className="movies-card__description">
         <h3 className="movies-card__title">{card.name}</h3>
-        <button className={`movies-card__heart ${card.isLiked ? "movies-card__heart_active" : ""
-          }`} />
+        <button className="movies-card__remove" />
         <p className="movies-card__time">{time}</p>
       </figcaption>
     </figure>
