81e1bd2494e5378e453bfd28be52b0f73ce9916c
Stas Smirnov <smistav@ya.ru>
2021-06-09 16:26:14 +0300

    add Form Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,52 @@
 import React from "react";
+import Logo from "../Logo/Logo";
+import { Link } from "react-router-dom";
 import './Login.css';
 
 function Login() {
   return (
-    <section>
-      Login
-    </section>
+    <main>
+      <form name="form_login" className="popup__container popup__container_form_login" noValidate>
+        <Link className="popup__link" to="/">
+          <Logo />
+        </Link>
+
+        <h4 className="popup__header">Рады видеть!</h4>
+        <div className="popup__input-container">
+          <p className="popup__input-header">E-mail</p>
+          <input
+            id="email-input"
+            name="email"
+            placeholder="Email"
+            className="popup__input"
+            type="email"
+            minLength="2"
+            maxLength="40"
+            required
+          />
+        </div>
+        <div className="popup__input-container">
+          <p className="popup__input-header">Пароль</p>
+          <input
+            id="password"
+            name="password"
+            placeholder="Пароль"
+            className="popup__input"
+            type="password"
+            minLength="2"
+            maxLength="100"
+            required
+          />
+        </div>
+        <button type="submit" name="button" className="popup__button">Войти</button>
+        <div className="popup__block-link">
+          <p className="popup__paragraph">Ещё не зарегистрированы?</p>
+          <Link to="signup" className="popup__link popup__link_position_bottom">
+            Регистрация
+          </Link>
+        </div>
+      </form>
+    </main>
   )
 }
 export default Login
