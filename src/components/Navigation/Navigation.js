b6b17ce6bfa7a017103185343e4e27c3a2c23540
Stas Smirnov <smistav@ya.ru>
2021-06-12 18:06:44 +0300

    fix Navigation tablet

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navigation.css';
-import './Navigation_768.css';
+import './NavigationTablet.css';
 import account_icon from "../../images/account-icon.svg";
-import menu_icon from '../../images/icon_menu_768.svg';
-import menu_icon_close from '../../images/icon_menu_768_close.svg'
+import menu_icon_tablet from '../../images/icon_menu_tablet.svg';
+import menu_icon_close from '../../images/icon_menu_tablet_close.svg'
 import Logo from '../Logo/Logo';
 
 function Navigation() {
@@ -22,36 +22,42 @@ function Navigation() {
           <NavLink to="/movies" activeClassName="menu__link_active" className="main__link menu__link">
             Фильмы
         </NavLink>
-          <NavLink to="/saved-movies" activeClassName="menu__link_active" className="main__link menu__link">
+          <NavLink to="/saved-movies" activeClassName="menu__link_active"
+            className="main__link menu__link">
             Сохраненные фильмы
         </NavLink>
         </div>
-        <NavLink to="/profile" activeClassName="menu__link_active" className="main__link menu__link menu__link-image">
+        <NavLink to="/profile" activeClassName="menu__link_active" className="main__link 
+        menu__link menu__link-image">
           Аккаунт
         <img src={account_icon} alt="иконка" className="main__link menu__account-icon" />
         </NavLink>
-        <div className="main__link menu__icon">
-          <img onClick={handleClick} src={menu_icon} alt="иконка" className="main__link" />
+        <div className="main__link menu__icon-tablet">
+          <img onClick={handleClick} src={menu_icon_tablet} alt="" className="main__link" />
         </div>
       </nav>
-      <nav onClick={handleClick} className={`menu_res_768 ${activeMenu ? "menu_res_768_visible" : ""}`}>
-        <NavLink exact to="/" activeClassName="menu__link_active_res_768" className="main__link menu__link_res_768">
+      <nav onClick={handleClick} className={`menu-tablet ${activeMenu ? "menu-tablet_visible" : ""}`}>
+        <NavLink exact to="/" activeClassName="menu-tablet__link_active" className="main__link 
+        menu-tablet__link">
           Главная
         </NavLink>
-        <div className='menu__block-link_res_768'>
-          <NavLink to="/movies" activeClassName="menu__link_active_res_768" className="main__link menu__link_res_768">
+        <div className='menu-tablet__block-link'>
+          <NavLink to="/movies" activeClassName="menu-tablet__link_active" className="main__link 
+          menu-tablet__link">
             Фильмы
       </NavLink>
-          <NavLink to="/saved-movies" activeClassName="menu__link_active_res_768" className="main__link menu__link_res_768">
+          <NavLink to="/saved-movies" activeClassName="menu-tablet__link_active"
+            className="main__link menu-tablet__link">
             Сохраненные фильмы
       </NavLink>
         </div>
-        <NavLink to="/profile" activeClassName="menu__link_active_res_768" className="main__link menu__link_res_768 menu__link-image_res_768">
+        <NavLink to="/profile" activeClassName="menu-tablet__link_active" className="main__link 
+        menu-tablet__link menu-tablet__link-image">
           Аккаунт
-      <img src={account_icon} alt="иконка" className="main__link menu__account-icon_res_768" />
+      <img src={account_icon} alt="" className="main__link menu-tablet__account-icon" />
         </NavLink>
-        <div className="main__link menu__icon_close">
-          <img onClick={handleClick} src={menu_icon_close} alt="иконка" className="main__link" />
+        <div className="main__link menu-tablet__icon-close">
+          <img onClick={handleClick} src={menu_icon_close} alt="" className="main__link" />
         </div>
       </nav>
     </>
