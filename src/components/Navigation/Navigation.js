7b26067eadc63c4aed46b4ae58d98a4ee60f38af
Stas Smirnov <smistav@ya.ru>
2021-06-13 14:19:32 +0300

    add Navigation at 320px

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 import './NavigationTablet.css';
+import './NavigationPhone.css'
 import account_icon from "../../images/account-icon.svg";
-import menu_icon_tablet from '../../images/icon_menu_tablet.svg';
-import menu_icon_close from '../../images/icon_menu_tablet_close.svg'
+import menu_icon_close from '../../images/icon_menu_close.svg'
 import Logo from '../Logo/Logo';
 
 function Navigation() {
   const [activeMenu, setActiveMenu] = useState(false);
+  const [activeMenuT, setActiveMenuT] = useState(false);
   function handleClick() {
-    activeMenu ? setActiveMenu(false) : setActiveMenu(true);
+    const pageWidth = document.documentElement.scrollWidth;
+    pageWidth === 320 ?
+      activeMenu ? setActiveMenu(false) : setActiveMenu(true) :
+      activeMenuT ? setActiveMenuT(false) : setActiveMenuT(true);
   }
   return (
     <>
@@ -32,11 +36,11 @@ function Navigation() {
           Аккаунт
         <img src={account_icon} alt="иконка" className="main__link menu__account-icon" />
         </NavLink>
-        <div className="main__link menu__icon-tablet">
-          <img onClick={handleClick} src={menu_icon_tablet} alt="" className="main__link" />
+        <div className="main__link menu__icon-tablet menu__icon-phone">
+          <button onClick={handleClick} className="main__link menu__button" />
         </div>
       </nav>
-      <nav onClick={handleClick} className={`menu-tablet ${activeMenu ? "menu-tablet_visible" : ""}`}>
+      <nav onClick={handleClick} className={`menu-tablet ${activeMenuT ? "menu-tablet_visible" : ""}`}>
         <NavLink exact to="/" activeClassName="menu-tablet__link_active" className="main__link 
         menu-tablet__link">
           Главная
@@ -57,6 +61,30 @@ function Navigation() {
       <img src={account_icon} alt="" className="main__link menu-tablet__account-icon" />
         </NavLink>
         <div className="main__link menu-tablet__icon-close">
+          <img onClick={handleClick} src={menu_icon_close} alt="" className="main__link" />
+        </div>
+      </nav>
+      <nav onClick={handleClick} className={`menu-phone ${activeMenu ? "menu-phone_visible" : ""}`}>
+        <NavLink exact to="/" activeClassName="menu-phone__link_active" className="main__link 
+        menu-phone__link">
+          Главная
+        </NavLink>
+        <div className='menu-phone__block-link'>
+          <NavLink to="/movies" activeClassName="menu-phone__link_active" className="main__link 
+          menu-phone__link">
+            Фильмы
+      </NavLink>
+          <NavLink to="/saved-movies" activeClassName="menu-phone__link_active"
+            className="main__link menu-phone__link">
+            Сохраненные фильмы
+      </NavLink>
+        </div>
+        <NavLink to="/profile" activeClassName="menu-phone__link_active" className="main__link 
+        menu-phone__link menu-phone__link-image">
+          Аккаунт
+      <img src={account_icon} alt="" className="main__link menu-phone__account-icon" />
+        </NavLink>
+        <div className="main__link menu-phone__icon-close">
           <img onClick={handleClick} src={menu_icon_close} alt="" className="main__link" />
         </div>
       </nav>
